07da2f493a9137b8f11e82e47e03c1d92fecb151
Popa Dorian <popa.dorian98@gmail.com>
2021-10-21 17:32:12 +0300

    First commit

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');               // Builder
+const web3 = new Web3(ganache.provider());  // Instance
+
+beforeEach(() => {
+    // Get a list of all accounts
+
+    // Use one of those accounts to deploy 
+    // the contract
+});
+
