ab3e04219a7356a4de34a6b1ba960cacea150c4c
Popa Dorian <popa.dorian98@gmail.com>
2021-10-21 17:57:05 +0300

    Accounts

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,10 +3,18 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');               // Builder
 const web3 = new Web3(ganache.provider());  // Instance
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
     // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy 
     // the contract
 });
 
+describe("Inbox", () => {
+    it("deploys a contract", () => {
+        console.log(accounts);
+    });
+});
