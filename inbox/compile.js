14a02a635dc823ded88b84ac84d1eb045abea668
Popa Dorian <popa.dorian98@gmail.com>
2021-10-21 18:13:59 +0300

    Contract deploy

--- inbox/compile.js
+++ inbox/compile.js
@@ -7,4 +7,4 @@ const inboxPath = path.resolve(
 );
 const source = fs.readFileSync(inboxPath, 'utf-8');
 
-module.exports = solc.compile(source, 1).contracts['Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Inbox'];;
