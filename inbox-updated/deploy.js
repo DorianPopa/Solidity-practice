b27eeb316c92adba9423282e95120750bd001bc3
Popa Dorian <popa.dorian98@gmail.com>
2021-10-22 16:39:11 +0300

    Updated to the latest version

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const { abi, evm } = require("./compile");
+
+const {
+	TEST_ACCOUNT_MNEMONIC,
+	INFURA_ENDPOINT_KOVAN,
+} = require("./config/config");
+
+provider = new HDWalletProvider(
+	TEST_ACCOUNT_MNEMONIC, 
+	INFURA_ENDPOINT_KOVAN
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log("Attempting to deploy from account", accounts[0]);
+
+	const result = await new web3.eth.Contract(abi)
+		.deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+		.send({ gas: "1000000", from: accounts[0] });
+
+	console.log("Contract deployed to", result.options.address);
+	provider.engine.stop();
+};
+
+deploy();
