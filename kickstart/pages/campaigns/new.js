b817f8c456d2650376aeb25df4133420e05f79c7
Popa Dorian <popa.dorian98@gmail.com>
2021-11-03 00:15:22 +0200

    Changed CampaignNew to a functional component

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,73 +1,73 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Form, Input, Message } from "semantic-ui-react";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
+import { useRouter } from "next/router";
 
-class CampaignNew extends Component {
-	state = {
+const CampaignNew = () => {
+	const router = useRouter();
+
+	const [state, setState] = useState({
 		minimumContribution: "0",
 		errorMessage: "",
 		loading: false,
-	};
+	});
 
-	onChangeMinimumContribution = (e) => {
+	const onChangeMinimumContribution = (e) => {
 		let newValue = e.target.value;
 		if (newValue === "") newValue = "0";
 
-		this.setState({ minimumContribution: newValue });
+		setState((prev) => ({ ...prev, minimumContribution: newValue }));
 	};
 
-	onSubmit = async (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
 		// If there's already an operation in progress, don't do anything
-		if (this.state.loading) return;
+		if (state.loading) return;
 
-		this.setState({ loading: true, errorMessage: "" });
+		setState((prev) => ({ ...prev, loading: true, errorMessage: "" }));
 
 		try {
 			const accounts = await web3.eth.getAccounts();
 			await factory.methods
-				.createCampaign(this.state.minimumContribution)
+				.createCampaign(state.minimumContribution)
 				.send({
 					from: accounts[0],
 				});
+
+			router.push("/");
 		} catch (err) {
-			this.setState({ errorMessage: err.message });
+			setState((prev) => ({ ...prev, errorMessage: err.message }));
 		}
 
-		this.setState({ loading: false });
+		setState((prev) => ({ ...prev, loading: false }));
 	};
 
-	render() {
-		return (
-			<>
-				<h3>Create a Campaign</h3>
-				<Form
-					onSubmit={this.onSubmit}
-					error={this.state.errorMessage.length !== 0}
-				>
-					<Form.Field>
-						<label>Minimum Contribution</label>
-						<Input
-							label="wei"
-							labelPosition="right"
-							placeholder="Minimum Contribution"
-							value={this.state.minimumContribution}
-							onChange={this.onChangeMinimumContribution}
-						/>
-					</Form.Field>
-					<Message
-						error
-						header="There was a problem with your submission"
-						content={this.state.errorMessage}
+	return (
+		<>
+			<h3>Create a Campaign</h3>
+			<Form onSubmit={onSubmit} error={state.errorMessage.length !== 0}>
+				<Form.Field>
+					<label>Minimum Contribution</label>
+					<Input
+						label="wei"
+						labelPosition="right"
+						placeholder="Minimum Contribution"
+						value={state.minimumContribution}
+						onChange={onChangeMinimumContribution}
 					/>
-					<Button primary type="submit" loading={this.state.loading}>
-						Create
-					</Button>
-				</Form>
-			</>
-		);
-	}
-}
+				</Form.Field>
+				<Message
+					error
+					header="There was a problem with your submission"
+					content={state.errorMessage}
+				/>
+				<Button primary type="submit" loading={state.loading}>
+					Create
+				</Button>
+			</Form>
+		</>
+	);
+};
 
 export default CampaignNew;
