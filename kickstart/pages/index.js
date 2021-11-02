c8912aa281860e3a651a0a66eeaf001746b77ddf
Popa Dorian <popa.dorian98@gmail.com>
2021-11-02 11:30:19 +0200

    Semantic UI added + NextJs entry point

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
+import { Card } from "semantic-ui-react";
+
 class CampaignIndex extends Component {
-	async componentDidMount() {
+	// Next.js related
+	static async getInitialProps() {
 		const campaigns = await factory.methods.getDeployedCampaigns().call();
-		console.log(campaigns);
+
+		return { campaigns };
+	}
+
+	renderCampaigns() {
+		const items = this.props.campaigns.map((address) => {
+			return {
+				header: address,
+				description: <a>View campaign</a>,
+				fluid: true,
+			};
+		});
+
+		return <Card.Group items={items} />;
 	}
 
 	render() {
-		return <div>Campaigns index</div>;
+		return <div>{this.renderCampaigns()}</div>;
 	}
 }
 
